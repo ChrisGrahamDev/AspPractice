06e08689b4f9601572620f5098e5626d382e955d
ChrisGrahamDev <55584900+ChrisGrahamDev@users.noreply.github.com>
2024-02-27 23:12:42 +0000

    Add files via upload

---
+++ ASP Practice/CompanyEmployees/CompanyEmployees/Program.cs
@@ -0,0 +1,46 @@
+using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    app.UseHsts();
+};
+
+app.UseStaticFiles();
+app.UseCors("CorsPolicy");
+
+app.UseRouting();
+
+app.UseHttpsRedirection();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
